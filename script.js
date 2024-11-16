b01bafb7454e9d693bad5234ff1dba276e56114f
arshad012 <forarshad99@gmail.com>
2024-11-16 13:55:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,41 +6,49 @@ function startPromiseChaining() {
 
 	const firstPromise = new Promise((resolve) => {
 		setTimeout(() => {
+			out.innerText = `Result: ${inputNum}`;
 			resolve(inputNum);
 		},2000)
 	})
 
 	firstPromise.then((res) => {
-		out.innerText = `Result: ${res}`;
+		res *= 2;
 		return new Promise((resolve) => {
 			setTimeout(() => {
 				out.innerText = `Result: ${res}`;
-				resolve(res*2);
+				resolve(res);
 			}, 1000)
 		})
 	})
 	.then((res) => {
+		res -= 3;
 		return new Promise((resolve) => {
 			setTimeout(() => {
 				out.innerText = `Result: ${res}`;
-				resolve(res-3);
+				resolve(res);
 			}, 1000)
 		})
 	})
 	.then((res) => {
+		res /= 2;
 		return new Promise((resolve) => {
 			setTimeout(() => {
 				out.innerText = `Result: ${res}`;
-				resolve(res+10);
+				resolve(res);
 			}, 1000)
 		})
 	})
 	.then((res) => {
+		res += 10;
 		return new Promise((resolve) => {
 			setTimeout(() => {
-				out.innerText = `Final Result: ${res}`;
+				out.innerText = `Result: ${res}`;
+				resolve(res);
 			}, 1000)
 		})
+	})
+	.then((res) => {
+		out.innerText = `Final Result: ${res}`;
 	})
 }
 
