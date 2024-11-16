4f68031d0487769f18f4ed03e1c3254d1742205b
arshad012 <forarshad99@gmail.com>
2024-11-16 13:14:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const out = document.getElementById('output');
+
+function startPromiseChaining() {
+	const inputNum = document.getElementBbyId('ip').value;
+
+	const firstPromise = new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(inputNum);
+		},2000)
+	})
+
+	firstPromise.then((res) => {
+		out.innerText = `Result: ${res}`;
+	})
+}
